994e34563450563256d621d97da29beea4eea56d
Anthony Carnero <37918263+acarn506@users.noreply.github.com>
2021-08-25 13:49:54 -0700

    Movie fetch hook

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import { Wrapper, Content, Banner } from './Header.styles'
 
 const Header = () => (
     <Wrapper>
-        <Content>
+        <Content> 
             <Banner>
-                <h1>React Cinema</h1>
+                <Link to='/'>
+                    <h1>React Cinema</h1>
+                </Link>
             </Banner>
+           
         </Content>
     </Wrapper>
 )
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import  { useParams } from 'react-router-dom'
 
-const Movie = () => <div>Movie</div>
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
+
+// Components
+import Grid from './Grid'
+import Spinner from './Spinner'
+
+// Hook
+import { useMovieFetch } from '../hooks/useMovieFetch'
+
+// Image 
+import NoImage from '../images/no_image.jpeg'
+
+const Movie = () => {
+    const { movieId } = useParams()
+    const {state: movie, loading, error} = useMovieFetch(movieId)
+    console.log('movie', movie)
+    
+    return (
+        <div>Movie</div>
+    )
+}
 
 export default Movie
